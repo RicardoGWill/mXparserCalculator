aff5963c79d690aa1aa13d77b676225a063efbd8
ricardogwill <ricardogwill@gmail.com>
2018-10-23 18:53:06 -0400

    Initial commit

---
+++ app/src/main/java/com/ricardogwill/mxparsercalculator/MainActivity.java
@@ -0,0 +1,202 @@
+package com.ricardogwill.mxparsercalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
+import org.mariuszgromada.math.mxparser.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView ioTextView, resultTextView;
+    Button equalsButton, buttonDecimal, button0, buttonClear, button1, button2, button3, button4,
+            button5, button6, button7, button8, button9, openParenthesisButton, closeParenthesisButton,
+            addButton, subtractButton, multiplyButton, divideButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewSetup();
+        onClickListenerSetup();
+    }
+
+    public void viewSetup() {
+        ioTextView = findViewById(R.id.io_textView);
+        resultTextView = findViewById(R.id.result_textView);
+        equalsButton = findViewById(R.id.equals_button);
+        buttonDecimal = findViewById(R.id.decimal_button);
+        button0 = findViewById(R.id.zero_button);
+        buttonClear = findViewById(R.id.clear_button);
+        button1 = findViewById(R.id.one_button);
+        button2 = findViewById(R.id.two_button);
+        button3 = findViewById(R.id.three_button);
+        button4 = findViewById(R.id.four_button);
+        button5 = findViewById(R.id.five_button);
+        button6 = findViewById(R.id.six_button);
+        button7 = findViewById(R.id.seven_button);
+        button8 = findViewById(R.id.eight_button);
+        button9 = findViewById(R.id.nine_button);
+        openParenthesisButton = findViewById(R.id.open_parenthesis_button);
+        closeParenthesisButton = findViewById(R.id.close_parenthesis_button);
+        addButton = findViewById(R.id.add_button);
+        subtractButton = findViewById(R.id.subtract_button);
+        multiplyButton = findViewById(R.id.multiply_button);
+        divideButton = findViewById(R.id.divide_button);
+    }
+
+    public void writeExpression(String value) {
+        String expressionString = ioTextView.getText().toString();
+        expressionString = expressionString + value;
+        ioTextView.setText(expressionString);
+    }
+
+    public void onClickListenerSetup() {
+
+        equalsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String expressionString = ioTextView.getText().toString();
+                Expression expression = new Expression(expressionString);
+                DecimalFormat decimalFormat = new DecimalFormat("0.####");
+                String result = decimalFormat.format(expression.calculate());
+                resultTextView.setText(result);
+                ioTextView.setText("");
+            }
+        });
+
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeExpression("0");
+            }
+        });
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeExpression("1");
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeExpression("2");
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeExpression("3");
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeExpression("4");
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeExpression("5");
+            }
+        });
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeExpression("6");
+            }
+        });
+
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeExpression("7");
+            }
+        });
+
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeExpression("8");
+            }
+        });
+
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeExpression("9");
+            }
+        });
+
+        buttonDecimal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeExpression(".");
+            }
+        });
+
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ioTextView.setText("");
+            }
+        });
+
+        openParenthesisButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeExpression("(");
+            }
+        });
+
+        closeParenthesisButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeExpression(")");
+            }
+        });
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeExpression("+");
+            }
+        });
+
+        subtractButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeExpression("-");
+            }
+        });
+
+        multiplyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeExpression("*");
+            }
+        });
+
+        divideButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeExpression("/");
+            }
+        });
+
+    }
+
+}
